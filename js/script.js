e3dc9b7f5c42b67f95daf2d651174b4247995374
KONSTRUKTEUR <raumerkunder@gmail.com>
2023-02-09 16:32:02 +0100

    Format code

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
 var date = new Date();
 var year = date.getFullYear();
 document.getElementById('curYr').innerHTML = year;
+
+
+const popover1 = new bootstrap.Popover('.index-popover1',  {
+    container: 'body'
+})
+const popover2 = new bootstrap.Popover('.index-popover2', {
+    container: 'body'
+})
+const popover3 = new bootstrap.Popover('.index-popover3', {
+    container: 'body'
+})
