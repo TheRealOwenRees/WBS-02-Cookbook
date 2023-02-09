2b28a9260ed90c1f163db9060c9674337497f3e5
Owen Rees <owenrees82@gmail.com>
2023-02-09 17:23:49 +0100

    tidy up of code

--- js/script.js
+++ js/script.js
@@ -2,5 +2,6 @@ var date = new Date();
 var year = date.getFullYear();
 document.getElementById('curYr').innerHTML = year;
 
+// bootstrap popover activation
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
